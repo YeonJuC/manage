8652f9b421589873f53a7900eda0ce707f885e02
yeonju <108509747+YeonJuC@users.noreply.github.com>
2026-01-15 17:18:57 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// ✅ 여기! index.css 없으니 styles.css로
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
