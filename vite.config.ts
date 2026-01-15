5f159444d2f363e561a2cc430ab2fac2b7691a52
yeonju <108509747+YeonJuC@users.noreply.github.com>
2026-01-15 17:54:42 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/manage/",
   plugins: [react()],
+  base: "/manage/", // ★ 레포 이름
 });
+
 
 
 
