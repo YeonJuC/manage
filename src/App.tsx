682b8a2e6cecf7d363832425ce1ce75b0679a686
yeonju <108509747+YeonJuC@users.noreply.github.com>
2026-01-15 17:12:04 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+export default function App() {
+  return <h1 style={{ color: "red" }}>HELLO</h1>;
+}
+
 import { useEffect, useMemo, useState } from "react";
 import "./styles.css";
 import type { Assignee, Cohort, Task } from "./types";
