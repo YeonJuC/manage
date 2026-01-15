bd93ecdbb05922f448a3e343a15662203bea347a
yeonju <108509747+YeonJuC@users.noreply.github.com>
2026-01-15 17:14:29 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+
+// ✅ 여기! index.css 없으니 styles.css로
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
